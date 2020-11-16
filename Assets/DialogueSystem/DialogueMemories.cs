6e2d92d55222aa27165018ec1bca67f4561f0d2e
DowdSean <dowds@rpi.edu>
2020-11-15 23:37:44 -0500

    Documentation for DialogueMemory
    Adds an explanation of the purpose of the DialogueMemory file.

--- Assets/DialogueSystem/DialogueMemories.cs
+++ Assets/DialogueSystem/DialogueMemories.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using InsomniaSystemTypes;
 
+
+/*
+	The purpose of this is to make it more open to a unity project as a whole rather just being an element of the 
+	Dialogue System. It acts as this middle man So now it can be editied and moved form scene to scene without 
+	needing to deal with the dialouge system monobehavior.
+*/
 public class DialogueMemories : MonoBehaviour
 {
 
